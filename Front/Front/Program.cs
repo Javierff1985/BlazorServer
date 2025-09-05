39d0f4ce233e7cf5e7e0430ec364aeaca5617291
Javierff1985 <javier.ucc@gmail.com>
2025-09-05 13:30:00 -0500

    avance en la grafica

--- Front/Front/Program.cs
+++ Front/Front/Program.cs
@@ -1,9 +1,10 @@
+﻿
 using Front.Components;
 using Front.Servicios;
 
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 builder.Services.AddScoped<ServiciosInkRoom>();
@@ -19,17 +20,11 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
 app.UseHttpsRedirection();
-
-
 app.UseAntiforgery();
-
 app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
-
 app.Run();
--- Front/Front/obj/Debug/net9.0/Front.AssemblyInfo.cs
+++ Front/Front/obj/Debug/net9.0/Front.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Front")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+deaeb5163beb80a4035206289d6526969b28cf11")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d67b23b0cee31bd1750c65098d9ea9d614472f30")]
 [assembly: System.Reflection.AssemblyProductAttribute("Front")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Front")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
