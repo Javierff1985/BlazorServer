d130c2cbb4744360c25b03255d2e537403b55c00
Javier Rodriguez Gomez <javier.ucc@gmail.com>
2025-09-07 09:28:12 -0500

    commit casa

--- Front/Front/obj/Debug/net9.0/Front.AssemblyInfo.cs
+++ Front/Front/obj/Debug/net9.0/Front.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Front")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89f9e25561e4e66e65c9eb45a269ac523fcd9145")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbaa2a5268cc0de10d1bc405c6f3eafdb16d467a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Front")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Front")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
